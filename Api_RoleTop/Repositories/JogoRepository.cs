7188ae2e53d966d8c20391e0a036efa6951ea923
Vitor <vitorleonel100@hotmail.com>
2020-07-20 18:35:15 -0300

    att

---
+++ Api_RoleTop/Repositories/JogoRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using Api_RoleTop.Domains;
+using Api_RoleTop.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_RoleTop.Repositories
+{
+    public class JogoRepository : RepositoryBase<Jogo> , IJogoRepository
+    {
+    }
+}
