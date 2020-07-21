7dd8d973109996540ca1cf97927b42ba61036d4c
FIC <FIC@info.local>
2020-07-21 15:50:12 -0300

    LoginView

--- RoleTopMobile/RoleTopMobile/App.xaml.cs
+++ RoleTopMobile/RoleTopMobile/App.xaml.cs
@@ -11,7 +11,7 @@ namespace RoleTopMobile
         {
             InitializeComponent();
 
-            MainPage = new ShowDetail();
+            MainPage = new LoginView();
         }
 
         protected override void OnStart()
---
+++ RoleTopMobile/RoleTopMobile/Views/LoginView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RoleTopMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginView : ContentPage
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new MainPage());
+        }
+
+        private void BtnCadastro_Clicked(object sender, EventArgs e)
+        {
+            //Navigation.PushAsync(new CadastroView());
+        }
+
+    }
+}
