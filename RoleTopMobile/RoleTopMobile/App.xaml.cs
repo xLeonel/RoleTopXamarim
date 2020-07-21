4e5ace7eb16313e116a84728ccdb417dcd88141b
FelipeSabino-del <felipedomingos2011@hotmail.com>
2020-07-21 13:12:36 -0300

    Imagens e splash adicionados

--- RoleTopMobile/RoleTopMobile/App.xaml.cs
+++ RoleTopMobile/RoleTopMobile/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RoleTopMobile.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace RoleTopMobile
         {
             InitializeComponent();
 
-            MainPage = new EstadioEvents();
+            MainPage = new Splash();
         }
 
         protected override void OnStart()
---
+++ RoleTopMobile/RoleTopMobile/Views/EventoDetail.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RoleTopMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EventoDetail : ContentPage
+    {
+        public EventoDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
