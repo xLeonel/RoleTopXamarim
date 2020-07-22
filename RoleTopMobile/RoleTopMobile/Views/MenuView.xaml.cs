2d127ec2eb5967309d00de232485174ef6ee2872
Fernando Monteiro <fer.monteirocalixto@gmail.com>
2020-07-22 20:46:10 -0300

    PageHome

---
+++ RoleTopMobile/RoleTopMobile/Views/MenuView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RoleTopMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuView : ContentPage
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+        }
+
+        private void BtnInicio_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new EstadioEvents());
+        }
+
+
+
+        private void BtnConfig_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new CadastroView());
+        }
+
+
+    }
+}
